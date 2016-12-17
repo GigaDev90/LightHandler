c66f5bc0ea54c181e060dafc5949f9008a700f3c
Gabriele <gabriele.gelardi@gmail.com>
2016-12-17 13:16:51 +0100

    led for monitor (ambilight)

--- main.c
+++ main.c
@@ -43,56 +43,31 @@ char isStillPressed = 0;
 bool status = OFF;
 bool bits[13];
 
-char c;
-
-void putch(unsigned char byte)
-{
-	/* output one byte */
-	TXREG = byte;
-	while (!TXSTAbits.TRMT); /* set when register is empty */
-}
+// char c;
+// 
+// void putch(unsigned char byte)
+// {
+// 	/* output one byte */
+// 	TXREG = byte;
+// 	while (!TXSTAbits.TRMT); /* set when register is empty */
+// }
 
 void setLum(int i)
 {
 	CCP1CONbits.DC1B = 00 | i;
 	CCPR1L = 00000000 | ( i >> 2  );
 }
-void onOff()
-{
-	if ( status == OFF )
-	{
-		for ( i = 0; i < 1024; i++)
-		{
-			setLum(i);
-			__delay_ms(1);
-		}
-
-		status	= ON;
-	}
-	else
-	{
-		i = 0000000000 | ( CCPR1L << 2 );
-		i = i | CCP1CONbits.DC1B;
-		for ( ; i >= 0; i--)
-		{
-			setLum(i);
-			__delay_ms(1);
-		}
-
-		status	= OFF;
-	}
-	//printf("%d %d \n\r",CCPR1L, CCP1CONbits.DC1B);
-
-}
 
 void increaseLum()
 {
-	x = 0000000000 | ( CCPR1L << 2 );
+	x = 0000000000 | ( CCPR1H << 2 );
 	x = x | CCP1CONbits.DC1B;
 
-	if ( x < 1024 )
+        x++;
+        
+	if ( x < 1024)
 	{
-		setLum(++x);
+		setLum(x);
 		__delay_ms(5);
 	}
 
@@ -100,39 +75,41 @@ void increaseLum()
 
 void decreaseLum()
 {
-	x = 0000000000 | ( CCPR1L << 2 );
+	x = 0000000000 | ( CCPR1H << 2 );
 	x = x | CCP1CONbits.DC1B;
 
-	if ( x > 0 )
+        x--;
+        
+	if ( x >= 0 )
 	{
-		setLum(--x);
+		setLum(x);
 		__delay_ms(5);
 	}
 
 }
 
-void maxLum()
-{
-	i = 0000000000 | ( CCPR1L << 2 );
-	i = i | CCP1CONbits.DC1B;
+// void maxLum()
+// {
+// 	i = 0000000000 | ( CCPR1H << 2 );
+// 	i = i | CCP1CONbits.DC1B;
+// 
+// 	for ( ; i < 1024; i++)
+// 	{
+// 		setLum(i);
+// 		//__delay_ms(1);
+// 	}
+// }
 
-	for ( ; i < 1024; i++)
-	{
-		setLum(i);
-		__delay_ms(1);
-	}
-}
-
-void minLum()
-{
-	i = 0000000000 | ( CCPR1L << 2 );
-	i = i | CCP1CONbits.DC1B;
-	for ( ; i > 0; i--)
-	{
-		setLum(i);
-		__delay_ms(1);
-	}	
-}
+// void minLum()
+// {
+// 	i = 0000000000 | ( CCPR1H << 2 );
+// 	i = i | CCP1CONbits.DC1B;
+// 	for ( ; i > 0; i--)
+// 	{
+// 		setLum(i);
+// 		__delay_ms(1);
+// 	}	
+// }
 
 // void decodeFromSerial( char c )
 // {
@@ -190,16 +167,16 @@ void minLum()
 // 
 // }
 
-void interrupt isr(void)
-{
-	// scatta ogni millisecondo
-	if (PIR1bits.RCIF == 1)
-	{
-		//c = RCREG;
-		//decodeFromSerial(c);
-
-	}
-}
+// void interrupt isr(void)
+// {
+// 	// scatta ogni millisecondo
+// 	if (PIR1bits.RCIF == 1)
+// 	{
+// 		//c = RCREG;
+// 		//decodeFromSerial(c);
+// 
+// 	}
+// }
 
 
 int main(void) {
@@ -211,48 +188,50 @@ int main(void) {
 
 	/* configure digital I/O */
 	TRISBbits.TRISB4 = 1;
-	TRISCbits.TRISC0 = 0;
-	TRISCbits.TRISC1 = 0;
+	TRISCbits.TRISC0 = 1;
+	TRISCbits.TRISC1 = 1;
 	TRISCbits.TRISC2 = 0;
 	TRISCbits.TRISC3 = 0;
 	TRISCbits.TRISC4 = 0;
 	TRISCbits.TRISC5 = 1;
 	TRISCbits.TRISC7 = 1;
-	TRISCbits.TRISC6 = 1;
+	TRISCbits.TRISC6 = 0;
 	TRISBbits.TRISB7 = 0; // TX PIN set to output
 	TRISBbits.TRISB5 = 1; // RX PIN set to input
 
-	PORTCbits.RC0 = 0;
-	PORTCbits.RC1 = 0;
-	PORTCbits.RC2 = 0;
+	//PORTCbits.RC0 = 1;
+	//PORTCbits.RC1 = 0;
+	PORTCbits.RC2 = 1;
 	PORTCbits.RC3 = 0;
 	PORTCbits.RC4 = 0;
+        PORTCbits.RC6 = 0;
+
 	//PORTCbits.RC5 = 1;
 
-	TXSTAbits.TX9 = 0; // 8 bit data
-	TXSTAbits.TXEN = 1; // enable transmitter
-	TXSTAbits.BRGH = 1; // high speed transmission
-
-	// setup UART receiver (porta seriale)
-	RCSTAbits.SPEN = 1; // enable serial port
-	RCSTAbits.RX9 = 0; // 8 bit data
-	RCSTAbits.CREN = 1; // enable receiver
+// 	TXSTAbits.TX9 = 0; // 8 bit data
+// 	TXSTAbits.TXEN = 1; // enable transmitter
+// 	TXSTAbits.BRGH = 1; // high speed transmission
+// 
+// 	// setup UART receiver (porta seriale)
+// 	RCSTAbits.SPEN = 1; // enable serial port
+// 	RCSTAbits.RX9 = 0; // 8 bit data
+// 	RCSTAbits.CREN = 1; // enable receiver
 
 	// baud rate generator control
-	BAUDCTLbits.BRG16 = 1; // 16 bit baud rate generator
+// 	BAUDCTLbits.BRG16 = 1; // 16 bit baud rate generator
 
-	// baud rate generator value
-	SPBRGH = 0;
-	SPBRG = 207;
+// 	// baud rate generator value
+// 	SPBRGH = 0;
+// 	SPBRG = 207;
 
 
 	/* configure timer */
-	OPTION_REGbits.T0CS = 0;  //tmr0 clock source
-	OPTION_REGbits.PS = 0b010;  //prescaler
-	OPTION_REGbits.PSA = 0; // prescaler is on tmr0
-	OPTION_REGbits.T0SE = 0;
-	INTCONbits.T0IF = 0;  // FLAG
-	INTCONbits.T0IE = 0;  // Enable interrupt on tmr0
+// 	OPTION_REGbits.T0CS = 0;  //tmr0 clock source
+// 	OPTION_REGbits.PS = 0b010;  //prescaler
+// 	OPTION_REGbits.PSA = 0; // prescaler is on tmr0
+// 	OPTION_REGbits.T0SE = 0;
+// 	INTCONbits.T0IF = 0;  // FLAG
+// 	INTCONbits.T0IE = 0;  // Enable interrupt on tmr0
 
 	INTCONbits.GIE = 1;  //global interrupt flag
 	INTCONbits.PEIE = 1;
@@ -275,10 +254,11 @@ int main(void) {
 
 	CCP1CONbits.P1M = 0b00; 
 	CCP1CONbits.CCP1M = 0b1100;
-	CCP1CONbits.DC1B = 0b00;
-	CCPR1L = 0;
+        CCPR1L = 0b00001111;
+	CCP1CONbits.DC1B = 0b11;
+        
 
-	PIR1bits.TMR2IF = 0; // FLAG
+	PIR1bits.TMR2IF = 0; // FLzAG
 	T2CONbits.T2CKPS = 0b01; // prescaler tmr2
 	//PIE1bits.TMR2IE = 1;  //enable interrupt on tmr2
 	T2CONbits.TMR2ON = 1; // enable tmr2
@@ -290,101 +270,18 @@ int main(void) {
 
 	for (;;)
 	{
-		attesa:
-
-		OPTION_REGbits.PS = 0b111;  //prescaler
- 		TMR0 = -195;
-		INTCONbits.T0IF = 0;	
-
-		while(	INTCONbits.T0IF == 1 )
-		{
-			if ( PORTBbits.RB4 == 0 )
-			{
-				TMR0 = -195;
-				INTCONbits.T0IF = 0;
-			}
-		}
-
-		
-		i = 0;
- 		TMR0 = 0;
-		INTCONbits.T0IF = 0;
-
-		while ( PORTBbits.RB4 == 1 )
-		{
-			
-			if ( INTCONbits.T0IF == 1 )
-			{
-				if ( i == 3 )
-				{
-					isStillPressed = 0;
-
-					while ( PORTBbits.RB4 == 1 );
-				}
-				else
-				{
-					i++;
-					TMR0 = 0;
-					INTCONbits.T0IF = 0;
-				}
-			}	
-		}
-
-		OPTION_REGbits.PS = 0b010;  //prescaler
-
-		for( i = 0; i < 13; i++ )
-		{
-			
-			__delay_us(1320);
-			bits[i] = PORTBbits.RB4;
-			TMR0 = -T_DELAY;
-			INTCONbits.T0IF = 0;
-			while (bits[i] == PORTBbits.RB4)
-			{	
-				if ( INTCONbits.T0IF == 1 )
-					goto attesa;
-			}
-		}
-
-		valore = 0;
-
-		if ( (bits[0] == 1) && (bits[2] == 0) ) //&& ( flip != bits[3] || flip == 2 ) )
-		{
-			int j = 6;
-			for ( i = 6; i < 13; i++ )
-			{
-				valore = valore | (bits[i] << j);
-				j--;
-			}
-
-			if ( valore == ONOFF )
-			{
-				if ( flip != bits[1] || !isStillPressed )
-					onOff();
-			}
-			else if ( status )
-			{
-				switch (valore)
-				{
-					case UP:
-						increaseLum();
-						break;
-					case DOWN:
-						decreaseLum();
-						break;
-					case LEFT:
-						minLum();
-						break;
-					case RIGHT:
-						maxLum();
-						break;
-				}
-				if ( valore <= 9 )
-					setLum( valore * 100 );
-			}
-
-			flip = bits[1];
-			isStillPressed = 1;
-		}				
+            
+            
+            if ( PORTCbits.RC0 == 1 && PORTCbits.RC1 != 1) {
+                increaseLum();
+                //PORTCbits.RC6 = 0;
+            }
+            
+            if ( PORTCbits.RC1 == 1 && PORTCbits.RC0 != 1) {
+                decreaseLum();
+                //PORTCbits.RC6 = 1;
+            }
+            
+            
 	}
 }
